ab5664cc925e000080e4166d33c4d0c935c912cf
ptitbob <github@shipstone.org>
2017-05-11 23:37:45 +0200

    Initialisation

---
+++ src/main/java/org/shipstone/sandbox/PlaywithspringandtddApplication.java
@@ -0,0 +1,12 @@
+package org.shipstone.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywithspringandtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywithspringandtddApplication.class, args);
+	}
+}
---
+++ src/test/java/org/shipstone/sandbox/PlaywithspringandtddApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shipstone.sandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaywithspringandtddApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
